6c1152c2060214ac21875aa1bf77840dea9c83ee
Test Man <ttestman4@gmail.com>
2018-11-26 21:38:28 +0530

    Fix #3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,57 @@
-import { TestBed, async } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { async, fakeAsync, tick, ComponentFixture } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
+
+import { ConfigureFn, configureTests } from '../lib/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(
+    async(() => {
+      const configure: ConfigureFn = testBed => {
+        testBed.configureTestingModule({
+          declarations: [AppComponent],
+          imports: [NoopAnimationsModule],
+          schemas: [NO_ERRORS_SCHEMA],
+        });
+      };
+
+      configureTests(configure).then(testBed => {
+        fixture = testBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+      });
+    })
+  );
+
+  it('should create the app',
+    async(() => {
+      const app = component;
+      expect(app).toBeTruthy();
+    })
+  );
+
+  it('snaps', () => {
+    expect(fixture).toMatchSnapshot();
   });
 
-  it(`should have as title 'spa'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('spa');
-  });
+  it(`should have as title 'spa'`,
+    async(() => {
+      const app = component;
+      expect(app.title).toEqual('spa');
+    })
+  );
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to spa!');
-  });
+  it(
+    'should render title in a h1 tag',
+    async(() => {
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('h1').textContent).toContain('Welcome to spa!');
+    })
+  );
 });
