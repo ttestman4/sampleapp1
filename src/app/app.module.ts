800942bf43cdb87fe074b73655e2fdabf34e5258
ttestman4 <ttestman4@gmail.com>
2018-12-13 07:12:29 +0530

    fix #11 Add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CustomLoggerLevel, NonFunctionalModule } from 'non-functional';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
@@ -11,13 +11,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     NonFunctionalModule.forRoot({
       serverLoggingUrl: '/api/logs',
       level: CustomLoggerLevel.DEBUG,
       serverLogLevel: CustomLoggerLevel.ERROR,
       disableConsoleLogging: false,
-    })
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
